2d9529bd7995513fbc4b52af11cd269ea87e742a
zain-bryxo <zain@bryxo.com>
2024-05-12 20:50:40 +0500

    first commit

---
+++ CHOOSE FILE INPUT SHOW/file choose 1/zain.js
@@ -0,0 +1,10 @@
+// var month = 'March';
+// alert(month + "'s winner is " + "25");
+
+// function greetUser(greeting) {
+//     var greeting = "Hello, there.";
+//     // greetUser(greeting);
+//     alert(greeting);
+// }
+
+
